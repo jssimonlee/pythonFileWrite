16ff7209bc9017bd2d8cbb039da9b0d1e7201c0c
Sim Jae-Seok <jssimonlee@gmail.com>
2023-10-08 14:23:45 +0900

    Update sttxtFile.py
    upload test

--- sttxtFile.py
+++ sttxtFile.py
@@ -1,7 +1,25 @@
 import streamlit as st
-file = open("TextFile.txt", "w")
-file.write("Hello~ \n")
-file.close()
-file = open("TextFile.txt", "r")
-a = file.readline()
-st.write(a)
+
+uploaded_file = st.file_uploader("업로드 파일을 선택하세요", type=extList)
+file_list = os.listdir()
+file_list_wanted = []
+for file in file_list:
+    root, extension = os.path.splitext(file)
+    if extension.replace('.','') in extList:
+        if file != 'requirements.txt':
+            file_list_wanted.append(file)
+selected_file = st.selectbox('확인하고 싶은 파일을 선택하세요.',file_list_wanted)
+submitted = st.button("파일내용확인")
+if selected_file and submitted:
+    try:
+        with open(selected_file,'r', encoding='utf-8') as f:
+            firstline = f.readline().replace("  ","{2칸}").replace("\t","{tab}")
+            secondline = f.readline().strip().replace("  ","{2칸}").replace("\t","{tab}")
+            dispTxt = f"""[1번째라인] {firstline}
+[2번째라인] {secondline}\n
+[총 라인수] {len(f.readlines())}"""
+                    st.success(dispTxt)
+                    # st.write(len(f.readlines()))
+            except:
+                st.warning('파일을 메모장에서 "utf-8"로 다시 저장하세요')
+
