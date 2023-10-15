edf7b76a9bc689d867ab4ff7cd97529f43ad551b
Sim Jae-Seok <jssimonlee@gmail.com>
2023-10-15 14:16:34 +0900

    Update sttxtFile.py
    password 안보이게

--- sttxtFile.py
+++ sttxtFile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-pw = st.text_input("비번을 넣으세요")
+pw = st.text_input("비번을 넣으세요", type="password")
 if pw == st.secrets["pw"]:    
     tab1, tab2, tab3 = st.tabs(["파일 업로드", "파일삭제", "파일 다운로드"])
     with tab1:
